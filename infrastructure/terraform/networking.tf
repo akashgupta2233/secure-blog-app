02ea0ac4a03fb025adef84a575e60fc13fbf7884
Akash <akashgupta9282@gmail.com>
2026-03-19 23:11:28 +0530

    refactor(terraform): replace hardcoded CIDR blocks with variables

--- infrastructure/terraform/networking.tf
+++ infrastructure/terraform/networking.tf
@@ -2,7 +2,7 @@
 # VPC
 # ------------------------
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
     Name = "secure-blog-vpc"
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
 # ------------------------
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
 
   tags = {
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_subnet" {
 # ------------------------
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.private_subnet_cidr
 
   tags = {
     Name = "secure-blog-private-subnet"
